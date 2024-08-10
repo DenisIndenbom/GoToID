e48e637570641c17e6d7a91e96d8d8459301499f
DenisIndenbom <denis.indenbom@yandex.ru>
2024-08-10 17:59:05 +0000

    Updated OAuth API (not checked)

--- oauth/routes/api.js
+++ oauth/routes/api.js
@@ -3,11 +3,15 @@ const prisma = require('../../lib/prisma')
 
 const router = express.Router() // Instantiate a new router
 
+function get_token(req) {
+    // Finds and responds with the user corresponding to the access token that was used to make the request
+    return req.headers.authorization.split(' ')[1]
+}
+
 router.get('/', (req, res) => res.status(200).json({ success: true }))
 
 router.get('/user', async (req, res) => {
-    // Finds and responds with the user corresponding to the access token that was used to make the request
-    let accessToken = req.headers.authorization.split(' ')[1]
+    let accessToken = get_token(req)
 
     const user = (await prisma.token.findFirst({
         where: {
@@ -18,7 +22,7 @@ router.get('/user', async (req, res) => {
         }
     })).user
 
-    res.status(200).json({
+    return res.status(200).json({
         user_id: user.id,
         username: user.username,
         first_name: user.firstName,
@@ -26,6 +30,75 @@ router.get('/user', async (req, res) => {
         type: user.type,
         createdAt: user.createdAt
     })
+})
+
+router.get('/email', async (req, res) => {
+    let accessToken = get_token(req)
+
+    const email = (await prisma.token.findFirst({
+        where: {
+            accessToken: accessToken
+        },
+        select: {
+            user: {
+                select: {
+                    Profile: {
+                        select: {
+                            email: true
+                        }
+                    }
+                }
+            }
+        }
+    })).user.Profile.email
+
+    return res.status(200).json({ email: email })
+})
+
+router.get('/telegram', async (req, res) => {
+    let accessToken = get_token(req)
+
+    const telegram = (await prisma.token.findFirst({
+        where: {
+            accessToken: accessToken
+        },
+        select: {
+            user: {
+                select: {
+                    Profile: {
+                        select: {
+                            telegram: true
+                        }
+                    }
+                }
+            }
+        }
+    })).user.Profile.telegram
+
+    return res.status(200).json({ telegram: telegram })
+})
+
+router.get('/avatar', async (req, res) => {
+    let accessToken = get_token(req)
+
+    const avatar = (await prisma.token.findFirst({
+        where: {
+            accessToken: accessToken
+        },
+        select: {
+            user: {
+                select: {
+                    Profile: {
+                        select: {
+                            avatarURL: true
+                        }
+                    }
+                }
+            }
+        }
+    })).user.Profile.avatarURL
+
+    return res.status(200).json({ avatar: avatar })
 })
 
 router.get('*', (req, res) => res.status(404).json({ success: false, description: 'Not found!' }))
