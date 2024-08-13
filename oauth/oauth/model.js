f5a0dfd6d3ff9f22c429ea560f878f3948088081
DenisIndenbom <denis.indenbom@yandex.ru>
2024-08-13 13:14:49 +0000

    Minor changes in code style

--- oauth/oauth/model.js
+++ oauth/oauth/model.js
@@ -3,149 +3,149 @@ const crypto = require('crypto')
 const prisma = require('../../lib/prisma')
 
 module.exports = {
-  getClient: async function (clientId, clientSecret) {
-    // query db for details with client
-    return prisma.client.findFirst(
-      {
-        where: {
-          clientId: clientId,
-          // clientSecret: clientSecret,
-        }
-      }
-    )
-  },
-  saveToken: async (token, client, user) => {
-    /* This is where you insert the token into the database */
-    const savedToken = await prisma.token.create({
-      data: {
-        accessToken: token.accessToken,
-        accessTokenExpiresAt: token.accessTokenExpiresAt,
-        refreshToken: token.accessToken, // NOTE this is only needed if you need refresh tokens down the line
-        refreshTokenExpiresAt: token.accessTokenExpiresAt,
-        clientId: client.clientId,
-        userId: user.user,
-      }
-    })
+    getClient: async function (clientId, clientSecret) {
+        // query db for details with client
+        return prisma.client.findFirst(
+            {
+                where: {
+                    clientId: clientId,
+                    // clientSecret: clientSecret,
+                }
+            }
+        )
+    },
+    saveToken: async (token, client, user) => {
+        /* This is where you insert the token into the database */
+        const savedToken = await prisma.token.create({
+            data: {
+                accessToken: token.accessToken,
+                accessTokenExpiresAt: token.accessTokenExpiresAt,
+                refreshToken: token.accessToken, // NOTE this is only needed if you need refresh tokens down the line
+                refreshTokenExpiresAt: token.accessTokenExpiresAt,
+                clientId: client.clientId,
+                userId: user.user,
+            }
+        })
 
-    return new Promise(resolve => resolve({ ...savedToken, client, user }))
-  },
-  getAccessToken: async accessToken => {
-    /* This is where you select the token from the database where the code matches */
-    if (!accessToken || accessToken === 'undefined') return false
-    const foundAccessToken = await prisma.token.findFirst({
-      where: {
-        accessToken: accessToken
-      },
-      select: {
-        accessToken: true,
-        accessTokenExpiresAt: true,
-        refreshToken: true,
-        refreshTokenExpiresAt: true,
-        client: true,
-        user: true
-      }
-    })
+        return new Promise(resolve => resolve({ ...savedToken, client, user }))
+    },
+    getAccessToken: async accessToken => {
+        /* This is where you select the token from the database where the code matches */
+        if (!accessToken || accessToken === 'undefined') return false
+        const foundAccessToken = await prisma.token.findFirst({
+            where: {
+                accessToken: accessToken
+            },
+            select: {
+                accessToken: true,
+                accessTokenExpiresAt: true,
+                refreshToken: true,
+                refreshTokenExpiresAt: true,
+                client: true,
+                user: true
+            }
+        })
 
-    const res = foundAccessToken
-    res["client"]["id"] = foundAccessToken.clientId
-    res["user"] = { user: foundAccessToken.user.id }
+        const res = foundAccessToken
+        res["client"]["id"] = foundAccessToken.clientId
+        res["user"] = { user: foundAccessToken.user.id }
 
-    return new Promise(resolve => resolve(foundAccessToken))
+        return new Promise(resolve => resolve(foundAccessToken))
 
-  },
-  getRefreshToken: async token => {
-    /* Retrieves the token from the database */
-    
-    const foundRefreshToken = await prisma.token.findFirst({
-      where: {
-        refreshToken: token
-      },
-      select: {
-        accessToken: true,
-        accessTokenExpiresAt: true,
-        refreshToken: true,
-        refreshTokenExpiresAt: true,
-        client: true,
-        user: true
-      }
-    })
+    },
+    getRefreshToken: async token => {
+        /* Retrieves the token from the database */
 
-    const res = foundRefreshToken
-    res["client"]["id"] = foundRefreshToken.clientId
-    res["user"] = { user: foundRefreshToken.user.id }
+        const foundRefreshToken = await prisma.token.findFirst({
+            where: {
+                refreshToken: token
+            },
+            select: {
+                accessToken: true,
+                accessTokenExpiresAt: true,
+                refreshToken: true,
+                refreshTokenExpiresAt: true,
+                client: true,
+                user: true
+            }
+        })
 
-    return new Promise(resolve => resolve(foundRefreshToken))
-  },
-  revokeToken: token => {
-    // TODO: Implement this
-    /* Delete the token from the database */
-    
-    if (!token || token === 'undefined') return new Promise(resolve => resolve(false))
+        const res = foundRefreshToken
+        res["client"]["id"] = foundRefreshToken.clientId
+        res["user"] = { user: foundRefreshToken.user.id }
 
-    return prisma.token.delete({ where: { refreshToken: token.refreshToken } })
-      .then(function (token) {
-        return !!token;
-      });
-  },
-  generateAuthorizationCode: (client, user, scope, callback) => {
-    /* generate authroization code */
+        return new Promise(resolve => resolve(foundRefreshToken))
+    },
+    revokeToken: token => {
+        // TODO: Implement this
+        /* Delete the token from the database */
 
-    const err = null;
-    const seed = crypto.randomBytes(256)
-    const code = crypto
-      .createHash('sha1')
-      .update(seed)
-      .digest('hex')
-    return callback(err, code);
-  },
-  saveAuthorizationCode: async (code, client, user) => {
-    /* This is where you store the access code data into the database */
+        if (!token || token === 'undefined') return new Promise(resolve => resolve(false))
 
-    return prisma.authCode.create({
-      data: {
-        authorizationCode: code.authorizationCode,
-        expiresAt: code.expiresAt,
-        clientId: client.clientId,
-        userId: user.user,
-        redirectUri: code.redirectUri
-      }
-    })
-  },
-  getAuthorizationCode: async (authorizationCode) => {
-    /* this is where we fetch the stored data from the code */
+        return prisma.token.delete({ where: { refreshToken: token.refreshToken } })
+            .then(function (token) {
+                return !!token;
+            });
+    },
+    generateAuthorizationCode: (client, user, scope, callback) => {
+        /* generate authroization code */
 
-    const authCode = await prisma.authCode.findFirst({
-      where: {
-        authorizationCode: authorizationCode
-      },
-      select: {
-        authorizationCode: true,
-        expiresAt: true,
-        // redirectUri: true,
-        clientId: true,
-        client: true,
-        user: true
-      }
-    })
+        const err = null;
+        const seed = crypto.randomBytes(256)
+        const code = crypto
+            .createHash('sha1')
+            .update(seed)
+            .digest('hex')
+        return callback(err, code);
+    },
+    saveAuthorizationCode: async (code, client, user) => {
+        /* This is where you store the access code data into the database */
 
-    const res = authCode
-    res["client"]["redirectUri"] = [authCode.client.redirectUris]
-    res["user"] = { user: authCode.user.id }
+        return prisma.authCode.create({
+            data: {
+                authorizationCode: code.authorizationCode,
+                expiresAt: code.expiresAt,
+                clientId: client.clientId,
+                userId: user.user,
+                redirectUri: code.redirectUri
+            }
+        })
+    },
+    getAuthorizationCode: async (authorizationCode) => {
+        /* this is where we fetch the stored data from the code */
 
-    return new Promise(resolve => resolve(res))
-  },
-  revokeAuthorizationCode: async code => {
-    /* This is where we delete codes */
-    
-    return prisma.authCode.delete({ where: { authorizationCode: code.authorizationCode } })
-      .then(function (authorizationCode) {
-        return !!authorizationCode;
-      });
-  },
-  verifyScope: (token, scope) => {
-    /* This is where we check to make sure the client has access to this scope */
+        const authCode = await prisma.authCode.findFirst({
+            where: {
+                authorizationCode: authorizationCode
+            },
+            select: {
+                authorizationCode: true,
+                expiresAt: true,
+                // redirectUri: true,
+                clientId: true,
+                client: true,
+                user: true
+            }
+        })
 
-    const userHasAccess = true  // return true if this user / client combo has access to this resource
-    return new Promise(resolve => resolve(userHasAccess))
-  }
+        const res = authCode
+        res["client"]["redirectUri"] = [authCode.client.redirectUris]
+        res["user"] = { user: authCode.user.id }
+
+        return new Promise(resolve => resolve(res))
+    },
+    revokeAuthorizationCode: async code => {
+        /* This is where we delete codes */
+
+        return prisma.authCode.delete({ where: { authorizationCode: code.authorizationCode } })
+            .then(function (authorizationCode) {
+                return !!authorizationCode;
+            });
+    },
+    verifyScope: (token, scope) => {
+        /* This is where we check to make sure the client has access to this scope */
+
+        const userHasAccess = true  // return true if this user / client combo has access to this resource
+        return new Promise(resolve => resolve(userHasAccess))
+    }
 }
--- oauth/routes/auth.js
+++ oauth/routes/auth.js
@@ -7,110 +7,110 @@ const comparePassword = require('../../methods').passwordHashing.comparePassword
 const router = express.Router() // Instantiate a new router
 
 router.get('/', (req, res) => {  // send back a simple form for the oauth
-  res.render('oauth/oauth_authenticate.html', {
-    base: 'base.html',
-    title: 'OAuth',
-    username: (req.session && req.session.username) ? req.session.username : ''
-  })
+    res.render('oauth/oauth_authenticate.html', {
+        base: 'base.html',
+        title: 'OAuth',
+        username: (req.session && req.session.username) ? req.session.username : ''
+    })
 })
 
 router.post('/authorize', async (req, res, next) => {
-  const username = req.body.username
-  const password = req.body.password
+    const username = req.body.username
+    const password = req.body.password
 
-  const params = [ // Send params back down
-    'client_id',
-    'redirect_uri',
-    'response_type',
-    'grant_type',
-    'state',
-  ]
-    .map(a => `${a}=${req.body[a]}`)
-    .join('&')
+    const params = [ // Send params back down
+        'client_id',
+        'redirect_uri',
+        'response_type',
+        'grant_type',
+        'state',
+    ]
+        .map(a => `${a}=${req.body[a]}`)
+        .join('&')
 
-  if (!username || !password) {
-    return res.redirect(`/oauth?success=false&${params}`)
-  }
-
-  const user = await prisma.user.findFirst({
-    where: {
-      username: username
+    if (!username || !password) {
+        return res.redirect(`/oauth?success=false&${params}`)
     }
-  })
 
-  const correct_password = user ? await comparePassword(password, user.password) : false
+    const user = await prisma.user.findFirst({
+        where: {
+            username: username
+        }
+    })
 
-  if (user && correct_password) {
-    req.body.user = { user: user.id }
-    return next()
-  }
+    const correct_password = user ? await comparePassword(password, user.password) : false
 
-  return res.redirect(`/oauth?success=false&${params}&wrong_password=true`)
+    if (user && correct_password) {
+        req.body.user = { user: user.id }
+        return next()
+    }
+
+    return res.redirect(`/oauth?success=false&${params}&wrong_password=true`)
 }, oauthServer.authorize({
-  authenticateHandler: {
-    handle: req => {
-      return req.body.user
+    authenticateHandler: {
+        handle: req => {
+            return req.body.user
+        }
     }
-  }
 }))
 
 router.post('/token', async (req, res, next) => {
-  const grant_type = req.body.grant_type
+    const grant_type = req.body.grant_type
 
-  const clientId = req.body.client_id
-  const clientSecret = req.body.client_secret
+    const clientId = req.body.client_id
+    const clientSecret = req.body.client_secret
 
-  const client = await prisma.client.findFirst(
-    {
-      where: {
-        clientId: clientId,
-        clientSecret: clientSecret,
-      }
-    }
-  )
-
-  // Check client exists
-  if (!client) return
-
-  if (grant_type === 'authorization_code') {
-    const code = req.body.code
-
-    const authCode = await prisma.authCode.findFirst(
-      {
-        where: {
-          authorizationCode: code,
-          clientId: clientId,
-          expiresAt: {
-            gte: new Date()
-          }
+    const client = await prisma.client.findFirst(
+        {
+            where: {
+                clientId: clientId,
+                clientSecret: clientSecret,
+            }
         }
-      }
     )
 
-    if (authCode) return next()
-  }
-  else if (grant_type === 'refresh_token') {
-    const token = req.body.refresh_token
+    // Check client exists
+    if (!client) return
 
-    const refresh_token = await prisma.token.findFirst({
-      where: {
-        refreshToken: token,
-        clientId: clientId,
-        refreshTokenExpiresAt: {
-          gte: new Date()
-        }
-      }
-    })
+    if (grant_type === 'authorization_code') {
+        const code = req.body.code
 
-    if (refresh_token) return next()
-  }
+        const authCode = await prisma.authCode.findFirst(
+            {
+                where: {
+                    authorizationCode: code,
+                    clientId: clientId,
+                    expiresAt: {
+                        gte: new Date()
+                    }
+                }
+            }
+        )
 
-  return res.status(400).send({error: 'Bad request'})
+        if (authCode) return next()
+    }
+    else if (grant_type === 'refresh_token') {
+        const token = req.body.refresh_token
+
+        const refresh_token = await prisma.token.findFirst({
+            where: {
+                refreshToken: token,
+                clientId: clientId,
+                refreshTokenExpiresAt: {
+                    gte: new Date()
+                }
+            }
+        })
+
+        if (refresh_token) return next()
+    }
+
+    return res.status(400).send({ error: 'Bad request' })
 }, oauthServer.token({
-  requireClientAuthentication: { // whether client needs to provide client_secret
-    'authorization_code': true,
-    'refresh_token': true
-  },
+    requireClientAuthentication: { // whether client needs to provide client_secret
+        'authorization_code': true,
+        'refresh_token': true
+    },
 }))  // Sends back token
 
 module.exports = router
