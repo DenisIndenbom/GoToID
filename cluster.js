1141626c01370697df7d99254f9dab4c0cb290be
DenisIndenbom <denis.indenbom@yandex.ru>
2024-08-16 12:42:43 +0000

    Fixed restart main process bug in cluster.js

--- cluster.js
+++ cluster.js
@@ -5,7 +5,6 @@ if (cluster.isMaster) {
 
     let cCPUs = workers > 0 ? workers : require('os').cpus().length
     let main_worker_id = 1
-    let last_worker_id = 0
 
     // Create a worker for each CPU
     for (let i = 0; i < cCPUs; i++) {
@@ -15,7 +14,6 @@ if (cluster.isMaster) {
     cluster.on('online', function (worker) {
         // Send start message to all workers and update last_worker_id. This is called from a worker's main thread
         worker.send({ topic: 'start', value: worker.id === main_worker_id })
-        last_worker_id = worker.id
 
         // log
         console.log(`Worker ${worker.process.pid} is online.`)
@@ -23,11 +21,12 @@ if (cluster.isMaster) {
 
     cluster.on('exit', function (worker, code, signal) {
         // Fork and report died. This is called by a worker that dies before the worker is removed
+        const new_worker = cluster.fork()
+
         if (worker.id === main_worker_id)
-            main_worker_id = last_worker_id + 1
+            main_worker_id = new_worker.id
 
-        cluster.fork()
-
+        // log
         console.log(`Worker ${worker.process.pid} died.`)
     })
 
