d74ac38ed387bb9335671def9445b0a3f405b256
DenisIndenbom <denis.indenbom@yandex.ru>
2025-03-07 09:27:56 +0000

    Deleted useless code

--- oauth/routes/oauth.js
+++ oauth/routes/oauth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const oauth_server = require('../oauth/server.js');
 
-const prisma = require('../../lib/prisma');
 const auth_handler = require('../../methods/is_auth.js');
 
 const router = express.Router(); // Instantiate a new router
@@ -46,43 +45,8 @@ router.post(
 
 router.post(
 	'/token',
-	async (req, res, next) => {
-		const grant_type = req.body.grant_type;
-
-		if (grant_type === 'authorization_code') {
-			const code = req.body.code;
-
-			const auth_code = await prisma.authCode.findFirst({
-				where: {
-					authorizationCode: code,
-					expiresAt: {
-						gte: new Date(),
-					},
-				},
-			});
-
-			if (auth_code) return next();
-		} else if (grant_type === 'refresh_token') {
-			const token = req.body.refresh_token;
-
-			const refresh_token = await prisma.token.findFirst({
-				where: {
-					refreshToken: token,
-					clientId: clientId,
-					refreshTokenExpiresAt: {
-						gte: new Date(),
-					},
-				},
-			});
-
-			if (refresh_token) return next();
-		}
-
-		return res.status(400).send({ error: 'Bad request' });
-	},
 	oauth_server.token({
 		requireClientAuthentication: {
-			// whether client needs to provide client_secret
 			authorization_code: true,
 			refresh_token: true,
 		},
